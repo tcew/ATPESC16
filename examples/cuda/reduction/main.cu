924805fd43c0791790c6535bae0c4a49fe153e41
TW <tim.warburton@gmail.com>
2016-08-01 16:59:23 -0500

    fixed errors and typos

--- examples/cuda/reduction/main.cu
+++ examples/cuda/reduction/main.cu
@@ -45,85 +45,85 @@ __global__ void partialSum(const int N,
   
 
 // same partial sum reduction, but with unrolled while loop
-__global__ void unrolledPartialSum(const int entries,
+__global__ void unrolledPartialSum(const int N,
 				   datafloat *u,
 				   datafloat *blocksum){
 
-  __shared__ datafloat s_red[BDIM];
+  __shared__ datafloat s_blocksum[BDIM];
 
   int t = threadIdx.x;  
   int b = blockIdx.x;
   const int n = b*blockDim.x + t;
 
-  s_red[t] = 0;
+  s_blocksum[t] = 0;
   
   if(n<N){
-    s_red[t] = u[n];
+    s_blocksum[t] = u[n];
   }
 
-  __syncthreads();  // barrier (make sure s_red is ready)
+  __syncthreads();  // barrier (make sure s_blocksum is ready)
 
-  // manually unrolled reduction (assumes BDIM=256)
+  // manually unrolled blocksumuction (assumes BDIM=256)
   if(BDIM>128) {
     if(t<128)
-      s_red[t] += s_red[t+128];
+      s_blocksum[t] += s_blocksum[t+128];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>64){
     if(t<64)
-      s_red[t] += s_red[t+64];
+      s_blocksum[t] += s_blocksum[t+64];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>32){
     if(t<32)
-      s_red[t] += s_red[t+32];
+      s_blocksum[t] += s_blocksum[t+32];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>16){
     if(t<16)
-      s_red[t] += s_red[t+16];
+      s_blocksum[t] += s_blocksum[t+16];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>8){
     if(t<8)
-      s_red[t] += s_red[t+8];
+      s_blocksum[t] += s_blocksum[t+8];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>4){
     if(t<4)
-      s_red[t] += s_red[t+4];
+      s_blocksum[t] += s_blocksum[t+4];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>2){
     if(t<2)
-      s_red[t] += s_red[t+2];
+      s_blocksum[t] += s_blocksum[t+2];
 
-    __syncthreads();  // barrier (make sure s_red is ready)
+    __syncthreads();  // barrier (make sure s_blocksum is ready)
   }
 
   if(BDIM>1){
     if(t<1)
-      s_red[t] += s_red[t+1];
+      s_blocksum[t] += s_blocksum[t+1];
   }
 
-  // store result of this block reduction
+  // store result of this block blocksumuction
   if(t==0)
-    red[b] = s_red[t];
+    blocksum[b] = s_blocksum[t];
 }
 
-void reduction(int N, datafloat tol, datafloat *h_u){
+void sum(int N, datafloat *h_u){
 
   // Device Arrays
   datafloat *c_u, *c_partialsum;
@@ -132,14 +132,14 @@ void reduction(int N, datafloat tol, datafloat *h_u){
   datafloat *h_partialsum;
 
   // number of thread-blocks to partial sum u
-  int B = (N+BDIM-1)/BDIM;
+  int GDIM = (N+BDIM-1)/BDIM;
 
   // allocate host array
-  h_partialsum = (datafloat*) calloc(B, sizeof(datafloat));
+  h_partialsum = (datafloat*) calloc(GDIM, sizeof(datafloat));
 
   // allocate device arrays
   cudaMalloc((void**) &c_u  , N*sizeof(datafloat));
-  cudaMalloc((void**) &c_partialsum , B*sizeof(datafloat));
+  cudaMalloc((void**) &c_partialsum , GDIM*sizeof(datafloat));
 
   // copy from h_u to c_u (HOST to DEVICE)
   cudaMemcpy(c_u ,  h_u ,  N*sizeof(datafloat), cudaMemcpyHostToDevice);
@@ -153,22 +153,23 @@ void reduction(int N, datafloat tol, datafloat *h_u){
 
   // perform reduction 10 times
   int Ntests = 10, test;
-  datafloat res;
+  datafloat psum = 0;
 
   for(test=0;test<Ntests;++test){
-    reduction <<< bdim, gdim >>> (N, c_u, c_partialsum);
+    // perform tree wise block reduction on DEVICE
+    unrolledPartialSum <<< GDIM, BDIM >>> (N, c_u, c_partialsum);
+
+    // copy array of partially summed values to HOST
+    cudaMemcpy(h_partialsum, c_partialsum, GDIM*sizeof(datafloat), cudaMemcpyDeviceToHost);
 
     // Finish reduce on host
-    cudaMemcpy(h_partialsum, c_partialsum, B*sizeof(datafloat), cudaMemcpyDeviceToHost);
-
-    datafloat psum = 0;
-    for(int n=0;n<B;++n){
+    psum = 0;
+    for(int n=0;n<GDIM;++n){
       psum += h_partialsum[n];
     }
-    printf("sum total = %g\n", psum);
-
   }
-  
+
+  // do timing
   cudaEventRecord(endEvent, 0);
   cudaEventSynchronize(endEvent);
 
@@ -176,8 +177,12 @@ void reduction(int N, datafloat tol, datafloat *h_u){
   float timeTaken;
   cudaEventElapsedTime(&timeTaken, startEvent, endEvent);
 
-  const datafloat avgTimePerTest = timeTaken/((datafloat) Ntests);
+  const datafloat avgTimePerTest = timeTaken/(1000.f*(datafloat) Ntests);
+  printf("average time per test = %g\n", avgTimePerTest);
 
+  // output summation result
+  printf("sum total = %g\n", psum);
+  
   // free device arrays
   cudaFree(c_u);
   cudaFree(c_partialsum);
@@ -202,12 +207,12 @@ int main(int argc, char** argv){
   datafloat *h_u   = (datafloat*) calloc(N, sizeof(datafloat));
   
   // initialize host array
-  for(int n = 0; i < N; ++i){
+  for(int n = 0;n < N; ++n){
     h_u[n] = 1;
   }
 
   // Solve discrete Laplacian
-  reduction(N, h_u);
+  sum(N, h_u);
 
   // Free the host array
   free(h_u);
